883868126ca63a261289e73cc462d55200a4903d
Mandytrajano90 <mandytrajano90@gmail.com>
2024-08-27 20:35:51 -0300

    refactoring: melhorando a lógica

--- desafio.py
+++ desafio.py
@@ -17,27 +17,35 @@ numero_saques = 0
 LIMITE_SAQUES = 3
 
 # Funções
-while True
+while True:
+
     opcao = input(menu)
 
     if opcao == "1":
         valor = float(input("Digite o valor a ser depositado: R$"))
-        saldo += valor
-        extrato += f"Depósito: R${valor:.2f}\n"
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R${valor:.2f}\n"
+        else:
+            print("Valor inválido.")
     
     elif opcao == "2":
-        if numero_saques < LIMITE_SAQUES:
             valor = float(input("Digite o valor a ser sacado: R$"))
-            if valor <= saldo + limite_diario:
+            if valor > limite:
+                print("Valor maior que o limite.")
+            elif valor > saldo:
+                print("Saldo insuficiente.")
+            elif numero_saques >= LIMITE_SAQUES:
+                print("Limite de saques diários atingido.")
+            else:
                 saldo -= valor
                 extrato += f"Saque: R${valor:.2f}\n"
                 numero_saques += 1
-            else:
-                print("Saldo insuficiente.")
+
     elif opcao == "3":
         print("##### EXTRATO #####")
         print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"Saldo: R${saldo:.2f}\nLimite: R${limite:.2f}\n{extrato}")
+        print(f"Saldo: R${saldo:.2f}\n")
         print("###################")
     
     elif opcao == "4":
